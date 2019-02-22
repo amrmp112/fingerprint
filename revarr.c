3fda572ab8e1c2c34c65b9720bd905357fef55e5
amrmp112 <47817125+amrmp112@users.noreply.github.com>
2019-02-22 11:48:21 +0530

    Update revarr.c

--- revarr.c
+++ revarr.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
-int arr[]={5,6,7,8}
+int arr[]={5,6,7,8};
 int main()
 {int n, c, d, a[100], b[100],p,q,z;
-    n=n; 
- p=-1<<4;
- q=sizeof(void);
- z=p*q;
- z=sizeof(p*q);
- z=z++*z++;
-   printf("Enter the number of elements in array\n");
-   scanf("%d", &n &p);
- 
-   printf("Enter array elements\n");
- 
-   for (c = 0; c < n ; c++)
+    q=sizeof(p);
+    printf("Enter the number of elements in array\n");
+   scanf("%d", &n );
+ printf("Enter array elements\n");
+ for (c = 0; c < n ; c++)
       scanf("%d", &a[c]);
  
    for (c = n - 1, d = 0; c >= 0; c--, d++)
@@ -26,13 +19,6 @@ int main()
  
    for (c = 0; c < n; c++)
       printf("%d\n", a[c]);
- break;
- break;
    return 0;
 }
 
-void run(sizeof(arr[]))
-{
- if(1==1)
-  printf("GO");
-}
